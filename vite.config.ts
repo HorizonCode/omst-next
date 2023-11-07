c6caf471f218731708b0cf5fbe97c79a73b25d49
Quetzalcoatl <18552186+HorizonCode@users.noreply.github.com>
2023-11-07 16:20:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://horizoncode.github.io/omst-next/",
   plugins: [svelte()],
 })
